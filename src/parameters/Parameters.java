fc4d6f37aba374fb02270851433cf0b7d0381719
Filippa Nilsson <filnils@kth.se>
2024-01-25 11:28:03 +0100

    Fix #18
    Add LIC interface.
    Add Parameters class and package dependencies.

---
+++ src/parameters/Parameters.java
@@ -0,0 +1,10 @@
+package parameters;
+/**
+ * A class to collect all parameters given in a single object.
+ * The LIC functions take the whole object and pick which fields to look at depending on the specification
+ * of the LIC.
+ */
+
+public class Parameters {
+    // TODO: fill this with the parameters from the assignment description
+}
