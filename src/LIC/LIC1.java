b6760b33a1adde0f20f99adfc19f57d1f8cbfd19
Filippa Nilsson <filnils@kth.se>
2024-01-29 14:35:48 +0100

    Fix #31
    Add asserts in LIC#0 and LIC#1 to make sure the requirements are met for each

--- src/LIC/LIC1.java
+++ src/LIC/LIC1.java
@@ -17,21 +17,24 @@ public class LIC1 implements LIC {
     * @return a boolean stating whether the 1:st LIC is satisfied
     */
    public boolean evaluate(Parameters p, int NUMPOINTS, double[] POINTSX, double[] POINTSY) {
-       for (int i = 0; i < NUMPOINTS-2; i++){
-           double x1 = POINTSX[i];
-           double y1 = POINTSY[i];
-           double x2 = POINTSX[i+1];
-           double y2 = POINTSY[i+1];
-           double x3 = POINTSX[i+2];
-           double y3 = POINTSY[i+2];
-           double dist1 = dist(x1, y1, x2, y2);
-           double dist2 = dist(x2, y2, x3, y3);
-           double dist3 = dist(x1, y1, x3, y3);
 
-           if (dist1 > p.RADIUS1*2 || dist2 > p.RADIUS1 *2 || dist3 > p.RADIUS1*2) {
-               return true;
-           }
-       }
+        assert(0 <= p.RADIUS1);
+
+        for (int i = 0; i < NUMPOINTS-2; i++){
+            double x1 = POINTSX[i];
+            double y1 = POINTSY[i];
+            double x2 = POINTSX[i+1];
+            double y2 = POINTSY[i+1];
+            double x3 = POINTSX[i+2];
+            double y3 = POINTSY[i+2];
+            double dist1 = dist(x1, y1, x2, y2);
+            double dist2 = dist(x2, y2, x3, y3);
+            double dist3 = dist(x1, y1, x3, y3);
+
+            if (dist1 > p.RADIUS1*2 || dist2 > p.RADIUS1 *2 || dist3 > p.RADIUS1*2) {
+                return true;
+            }
+        }
        return false;
    }
 
