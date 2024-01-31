32d64beeca237bc5f6164a8809a55a6a0c78ba4d
Anna Mårtensson <anna.m.maartensson@gmail.com>
2024-01-31 08:24:47 +0100

    Fix #51
    Fix bug in LIC#10 so that it handles invalid parameter input by raising an exception instead of returning false.
    Modify existing tests and add one new test accordingly to reflect correct behavior.

--- src/tests/LIC10Test.java
+++ src/tests/LIC10Test.java
@@ -6,10 +6,15 @@ import LIC.*;
 
 public class LIC10Test {
     Parameters p = new Parameters();
-    
+
+    /**
+     * INVALID INPUT TESTS
+     * Tests that evaluate function raises assertion error when given invalid parameter input.
+     */
+
     @Test
-    public void assertThatValidCriteriaReturnTrue() {
-        p.E_PTS = 1;
+    public void assertThatEPTSLessThan1ThrowsException() {
+        p.E_PTS = 0;
         p.F_PTS = 1;
         p.AREA1 = 3;
         int NUMPOINTS = 8;
@@ -17,21 +22,52 @@ public class LIC10Test {
         double[] POINTSX = {0, 1, 2, 3, 4, 5, 6, 7};
         double[] POINTSY = {0, 1, 1.5, 2, 2.5, 1.5, 0, 1};
         LIC10 lic10 = new LIC10();
-        assertTrue(lic10.evaluate(p, NUMPOINTS, POINTSX, POINTSY));
+        assertThrows(AssertionError.class, () -> {lic10.evaluate(p,NUMPOINTS,POINTSX,POINTSY);});
     }
 
     @Test
-    public void assertThatInvalidCriteriaReturnFalse() {
+    public void assertThatFPTSLessThan1ThrowsException() {
         p.E_PTS = 1;
-        p.F_PTS = 1;
+        p.F_PTS = 0;
         p.AREA1 = 3;
         int NUMPOINTS = 8;
 
         double[] POINTSX = {0, 1, 2, 3, 4, 5, 6, 7};
-        double[] POINTSY = {0, 1, 0, 1, 0, 1, 0, 1};
+        double[] POINTSY = {0, 1, 1.5, 2, 2.5, 1.5, 0, 1};
         LIC10 lic10 = new LIC10();
-        assertFalse(lic10.evaluate(p, NUMPOINTS, POINTSX, POINTSY));
+        assertThrows(AssertionError.class, () -> {lic10.evaluate(p,NUMPOINTS,POINTSX,POINTSY);});
     }
+
+    @Test
+    public void assertThatEPTSPlusFPTSTooLargeThrowsException() {
+        p.E_PTS = 2;
+        p.F_PTS = 4;
+        p.AREA1 = 3;
+        int NUMPOINTS = 8;
+
+        double[] POINTSX = {0, 1, 2, 3, 4, 5, 6, 7};
+        double[] POINTSY = {0, 1, 1.5, 2, 2.5, 1.5, 0, 1};
+        LIC10 lic10 = new LIC10();
+        assertThrows(AssertionError.class, () -> {lic10.evaluate(p,NUMPOINTS,POINTSX,POINTSY);});
+    }
+
+    @Test
+    public void assertThatAreaLessThan0ThrowsException() {
+        p.E_PTS = 1;
+        p.F_PTS = 1;
+        p.AREA1 = -1;
+        int NUMPOINTS = 8;
+
+        double[] POINTSX = {0, 1, 2, 3, 4, 5, 6, 7};
+        double[] POINTSY = {0, 1, 1.5, 2, 2.5, 1.5, 0, 1};
+        LIC10 lic10 = new LIC10();
+        assertThrows(AssertionError.class, () -> {lic10.evaluate(p,NUMPOINTS,POINTSX,POINTSY);});
+    }
+
+    /**
+     * FAILING TESTS
+     * Tests that evaluate function returns false when input does not satisfy LIC #10.
+     */
 
     @Test
     public void assertThatTooFewPointsReturnFalse() {
@@ -47,8 +83,26 @@ public class LIC10Test {
     }
 
     @Test
-    public void assertThatEPTSLessThan1ReturnFalse() {
-        p.E_PTS = 0;
+    public void assertThatInvalidCriteriaReturnFalse() {
+        p.E_PTS = 1;
+        p.F_PTS = 1;
+        p.AREA1 = 3;
+        int NUMPOINTS = 8;
+
+        double[] POINTSX = {0, 1, 2, 3, 4, 5, 6, 7};
+        double[] POINTSY = {0, 1, 0, 1, 0, 1, 0, 1};
+        LIC10 lic10 = new LIC10();
+        assertFalse(lic10.evaluate(p, NUMPOINTS, POINTSX, POINTSY));
+    }
+
+    /**
+     * PASSING TESTS
+     * Tests that evaluate function returns true when input satisfies LIC #10. 
+     */
+    
+    @Test
+    public void assertThatValidCriteriaReturnTrue() {
+        p.E_PTS = 1;
         p.F_PTS = 1;
         p.AREA1 = 3;
         int NUMPOINTS = 8;
@@ -56,34 +110,6 @@ public class LIC10Test {
         double[] POINTSX = {0, 1, 2, 3, 4, 5, 6, 7};
         double[] POINTSY = {0, 1, 1.5, 2, 2.5, 1.5, 0, 1};
         LIC10 lic10 = new LIC10();
-        assertFalse(lic10.evaluate(p, NUMPOINTS, POINTSX, POINTSY));
+        assertTrue(lic10.evaluate(p, NUMPOINTS, POINTSX, POINTSY));
     }
-
-    @Test
-    public void assertThatFPTSLessThan1ReturnFalse() {
-        p.E_PTS = 1;
-        p.F_PTS = 0;
-        p.AREA1 = 3;
-        int NUMPOINTS = 8;
-
-        double[] POINTSX = {0, 1, 2, 3, 4, 5, 6, 7};
-        double[] POINTSY = {0, 1, 1.5, 2, 2.5, 1.5, 0, 1};
-        LIC10 lic10 = new LIC10();
-        assertFalse(lic10.evaluate(p, NUMPOINTS, POINTSX, POINTSY));
-    }
-
-    @Test
-    public void assertThatEPTSPlusFPTSGreaterThanNumPointsReturnFalse() {
-        p.E_PTS = 2;
-        p.F_PTS = 4;
-        p.AREA1 = 3;
-        int NUMPOINTS = 8;
-
-        double[] POINTSX = {0, 1, 2, 3, 4, 5, 6, 7};
-        double[] POINTSY = {0, 1, 1.5, 2, 2.5, 1.5, 0, 1};
-        LIC10 lic10 = new LIC10();
-        assertFalse(lic10.evaluate(p, NUMPOINTS, POINTSX, POINTSY));
-    }
-
-    
 }
