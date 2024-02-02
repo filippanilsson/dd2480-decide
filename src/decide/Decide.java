a61bd6649bbe7749637ad4602dc177dfabc2ae7a
Emil Hultcrantz <90456354+Frequinzy@users.noreply.github.com>
2024-02-02 16:20:42 +0100

    Document the Decide class (#88)
    * Document the Decide class
    
    Fix #85
    
    * Fix spelling mistakes

--- src/decide/Decide.java
+++ src/decide/Decide.java
@@ -5,7 +5,22 @@ import FUV.FUV;
 import LIC.*;
 import PUM.PUM;
 
+/**
+ * Makes the final decision on whether to allow a launch or not. If the input parameters 
+ * are invalid this will throw an exception. Returns it's answer in the form of
+ * "YES" or "NO" in standard output.
+ */
 public class Decide {
+    /**
+     * Decides whether to allow a launch or not. 
+     * NOTE: Returns its answer in the form of "YES" or "NO" in standard output.
+     * @param NUMPOINTS the amount of points
+     * @param POINTSX an array of the x-coordinates of all points
+     * @param POINTSY an array of the y-coordinates of all points
+     * @param parameters Parameters object that contains all constant values
+     * @param LCM the logical connection matrix
+     * @param PUV Preliminary Unlocking Vector, a boolean array with 15 elements
+     */
     public static void DECIDE(int NUMPOINTS, double[] POINTSX, double[] POINTSY,
         Parameters parameters, String[][] LCM, boolean[] PUV) {
         LIC[] lics = {
